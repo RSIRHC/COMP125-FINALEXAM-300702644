75f9d1261f4bb5923bbde2ca0a17a6f45f1e1573
R.SIRHC <christopher.r99@live.com>
2016-08-17 12:48:13 -0400

    Innitial Commit

---
+++ Scripts/app.js
@@ -0,0 +1,30 @@
+/*
+ Author: Christopher Ritchil 
+ Student # : 300702644
+Date Created: August 17, 2016
+ Date updated: August 17, 2016
+ Description: COMP125-Assignment#3-JSON DATA
+ */
+
+var core;
+(function (core) {
+    "use strict";
+    var canvas;
+    var stage;
+    // app entry function
+    function init() {
+        canvas = document.getElementById("canvas");
+        stage = new createjs.Stage(canvas);
+        stage.enableMouseOver(20);
+        createjs.Ticker.framerate = 60;
+        createjs.Ticker.on("tick", gameLoop);
+        main();
+    }
+    function gameLoop() {
+        stage.update();
+    }
+    function main() {
+    }
+    window.addEventListener("load", init);
+})(core || (core = {}));
+//# sourceMappingURL=app.js.map
